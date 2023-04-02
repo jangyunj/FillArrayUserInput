e976847ee75e9fc5a10bfbc9e090230593957bec
Yun Jang <jang.yunj7@gmail.com>
2023-04-01 21:45:51 -0400

    added quit option

--- main.cpp
+++ main.cpp
@@ -6,17 +6,27 @@ int main()
 
     std::string foods[5];
     int size = sizeof(foods) / sizeof(foods[0]);
+    std::string temp;
 
     for (int i = 0; i < size; i++)
     {
-        std::cout << "Enter a food you like #" << i + 1 << ": ";
-        std::getline(std::cin, foods[i]);
+        std::cout << "Enter a food you like or 'q' to quit for #" << i + 1 << ": ";
+        std::getline(std::cin, temp);
+
+        if (temp == "q")
+        {
+            break;
+        }
+        else
+        {
+            foods[i] = temp;
+        }
     }
 
     std::cout << "You like the following food: \n";
 
-    for (std::string food : foods)
+    for (int i = 0; !foods[i].empty(); i++) // This should not display any elements that are empty
     {
-        std::cout << food << '\n';
+        std::cout << foods[i] << '\n';
     }
 }
